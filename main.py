c7ec1cd3cd3efce8201591d08cac104b4879a2dd
pythonista101 <123813803+pythonista101@users.noreply.github.com>
2023-03-07 20:08:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -188,3 +188,5 @@ with salary_pred:
     if button('Genereer'):
         percent, salarisS = salary_prediction(des, ex, stat, la_pe, la_be, size, rat)
         subheader(f'Het voorspelde salaris is {salarisS}, dat is {percent}% ten opzichte van het gdp')
+    text("Deze website is gemaakt door: ....")
+    image(qrcode)
