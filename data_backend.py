52faff16df440e7ddf67858ee157e978f1eb37a0
pythonista101 <123813803+pythonista101@users.noreply.github.com>
2023-03-07 18:14:46 +0100

    Add files via upload

---
+++ data_backend.py
@@ -0,0 +1,214 @@
+import pandas as pd
+import numpy as np
+
+# importing
+data = pd.read_csv(
+    r"C:\Users\daanh\Data Science\Cases\Case2\Data_Science_Fields_Salary_Categorization.csv",
+    index_col=0
+)
+codes = pd.read_excel('codes.xlsx')
+
+raw = data.copy()
+
+# dtypes
+data.Salary_In_Rupees = data.Salary_In_Rupees.str.replace(',', '').astype('float')
+data.Working_Year = data.Working_Year.astype('int64')
+
+eur_to_rup = {
+    2020: 85.22,
+    2021: 88.24,
+    2022: 82.76
+}
+
+
+def conversion(rup, year):
+    return round(rup / eur_to_rup[year], 2)
+
+
+vconv = np.vectorize(conversion)
+
+data['Salary_eur'] = vconv(data.Salary_In_Rupees, data.Working_Year)
+
+data = data.merge(codes, how='left', left_on='Company_Location', right_on='2_digit')
+
+gdp = pd.read_csv('gdp_per_capita.csv')
+d = gdp[['Country Name', 'Code', '2020']].rename(columns = {'2020':'BBP per hoofd'}).dropna()
+gdp = gdp[['Code', '2020']]
+
+
+
+
+
+data = data.merge(gdp, how='left', left_on='3_digit', right_on='Code')
+
+usd_to_eur = 0.877  # average exchange rate 2020
+
+data = data.rename(columns={'2020': 'gdp_euro'})
+data["gdp_euro"] = usd_to_eur * data["gdp_euro"]
+data["gdp_euro"] = np.around(data["gdp_euro"], 2)
+data['percentage'] = np.around(data['Salary_eur'] / data['gdp_euro'] * 100, 2)
+
+data['expat'] = np.where(data.Employee_Location == data.Company_Location, 'No', 'Yes')
+
+data = data[['Working_Year', 'Designation', 'Experience', 'Employment_Status',
+             'Employee_Location', 'Company_Location', 'expat',
+             'Company_Size', 'Remote_Working_Ratio', 'Salary_eur',
+             'gdp_euro', 'percentage', 'Country']]
+data_kasper = data.copy()
+
+x_kasper=pd.DataFrame(pd.Series(data_kasper.groupby('Designation').count().sort_values('Working_Year', ascending=False).index))
+y_kasper=pd.DataFrame(data_kasper.groupby('Designation')['Salary_eur'].mean())
+final_kasper=x_kasper.merge(y_kasper, on='Designation', how='left')
+
+data = data.rename(columns={
+    'Working_Year': 'Jaar',
+    # 'Designation':'Beroep',
+    # 'Experience':'Ervaring',
+    'Employment_Status': 'Status',
+    'Employee_Location': 'Land persoon',
+    'Company_Location': 'Land bedrijf',
+    'Company_Size': 'Grootte',
+    'Remote_Working_Ratio': 'Thuiswerk ratio',
+    'Salary_eur': 'Salaris €',
+    'gdp_euro': 'GDP €',
+    'percentage': '%',
+    'Country': 'Land'
+})
+
+
+def encoder(dataframe, column):
+    col = dataframe[column]
+
+    trans = {}
+    count = 0
+    for a in col:
+        if a not in trans.keys():
+            trans[a] = count
+            count += 1
+
+    copyseries = col.copy()
+    for index, element in enumerate(col):
+        copyseries[index] = trans[element]
+
+    dataframe[column] = copyseries
+
+    backwards = {}
+
+    for a in trans:
+        value = trans[a]
+        backwards[value] = a
+
+    return trans, backwards
+
+
+train_data = data.copy()
+
+desig_fwd, desig_bwd = encoder(train_data, 'Designation')
+status_fwd, status_bwd = encoder(train_data, 'Status')
+ex_fwd, ex_bwd = encoder(train_data, 'Experience')
+cp_fwd, cp_bwd = encoder(train_data, 'Land persoon')
+cc_fwd, cc_bwd = encoder(train_data, 'Land bedrijf')
+expat_fwd, expat_bwd = encoder(train_data, 'expat')
+size_fwd, size_bwd = encoder(train_data, 'Grootte')
+
+train_data = train_data[['Jaar', 'Designation', 'Experience', 'Status', 'Land persoon',
+                         'Land bedrijf', 'expat', 'Grootte', 'Thuiswerk ratio',
+                         'GDP €', '%', 'Salaris €']]
+
+X = train_data[['Designation', 'Experience', 'Status', 'Land persoon',
+                'Land bedrijf', 'expat', 'Grootte', 'Thuiswerk ratio']]
+Y = train_data[['%', 'Salaris €']]
+
+from sklearn import tree
+
+clf = tree.DecisionTreeRegressor()
+clf = clf.fit(X, Y)
+
+
+def salary_prediction(des, ex, stat, la_pe, la_be, size, rat):
+    if la_pe == la_be:
+        expat = 'No'
+    else:
+        expat = 'Yes'
+
+    x = pd.DataFrame({
+        'Designation': [desig_fwd[des]],
+        'Experience': [ex_fwd[ex]],
+        'Status': [status_fwd[stat]],
+        'Land persoon': [cp_fwd[la_pe]],
+        'Land bedrijf': [cc_fwd[la_be]],
+        'expat': [expat_fwd[expat]],
+        'Grootte': [size_fwd[size]],
+        'Thuiswerk ratio': [rat]
+    })
+    return clf.predict(x)[0]
+
+
+# some variabelen
+
+stat_dict = {'Fulltime': 'FT', 'Parttime': 'PT', 'Oproepbasis': 'CT', 'Freelance': 'FL'}
+ex_dict = {'Entry': 'EN', 'Medior': 'MI', 'Senior/Expert': 'SE', 'Executive': 'EX'}
+size_dict = {"Groot": 'L', "Medium": 'M', "Klein": 'S'}
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+ax.hist(data['Salaris €'])
+
+from PIL import Image
+
+salary_image = Image.open('salaris_ss.png')
+intro_image = Image.open('Intro afbeelding.png')
+
+
+with open('text/titel.txt', 'r') as file:
+    t_titel = file.read().replace('\n', '')
+
+with open('text/introductie.txt', 'r') as file:
+    t_introductie = file.read()
+with open('text/feature_engineering1.txt', 'r') as file:
+    t_feature_1 = file.read()
+with open('text/feature_engineering2.txt', 'r') as file:
+    t_feature_2 = file.read()
+with open('text/feature_engineering3.txt', 'r') as file:
+    t_feature_3 = file.read()
+
+
+import plotly.express as px
+gdp_map_bol = px.choropleth(d, locations='Code', color='BBP per hoofd',
+                        projection='orthographic')
+gdp_map_plat = px.choropleth(d, locations='Code', color='BBP per hoofd',
+                        projection='equirectangular')
+gdp_treemap= px.treemap(d, path = ['Country Name', 'BBP per hoofd'], values = 'BBP per hoofd', color = 'BBP per hoofd')
+
+
+
+fig2, ax3 = plt.subplots()
+ax3.grid()
+ax3.bar(eur_to_rup.keys(), eur_to_rup.values())
+
+min = 80
+max = 90
+ax3.set_ylim(min, max)
+ax3.set_xticks([2020,2021,2022])
+yticks = np.linspace(max,min,8)
+ax3.set_yticks(yticks)
+ax3.set_title('Roepie naar euro gemiddelde koerswaarde per jaar',
+              fontsize=15, y=1.06)
+ax3.set_ylabel('Aantal roepie')
+ax3.set_xlabel('Jaar')
+
+
+data_kasper=data_kasper[data_kasper.groupby('Designation')['Designation'].transform('count').ge(7)]
+kasper = px.bar(data_frame=data_kasper.groupby(['Designation']).mean().reset_index(),
+    x="Designation",
+    y="Salary_eur",
+    color='Designation',
+    text_auto='.2s',
+    labels={'Salary_eur':'Salaris in euro per jaar', 'Designation':'Beroep'},height=500,
+    title="Salaris per jaar per beroep in de US")
+kasper.add_hline(y=data_kasper['gdp_euro'].mean() , line_width=3, line_dash="dash", line_color="black")
+
+
+
+
---
+++ main.py
@@ -0,0 +1,190 @@
+import ftfy
+from streamlit import *
+from data_backend import *
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+titel = container()
+introductie = container()
+api_uitleg = container
+data_analyse = container()
+head = container()
+feature_1 = container()
+feature_2 = container()
+dataset_2 = container()
+gdp_merge = container()
+feature_engineering = container()
+visualisatie = container()
+salary_pred = container()
+
+
+with titel:
+    title("Analyse van salarissen in de data science wereld")
+    image(intro_image)
+with introductie:
+    write(ftfy.fix_encoding(t_introductie))
+    header('Data importeren via de kaggle API')
+    write('Hier gaan we uitleggen hoe we de data via de kaggle api hebben geimporteerd')
+
+    """import kaggle
+    api=kaggle.api()
+    """
+with data_analyse:
+    header('Data analyse')
+    write(raw.head())
+    subheader('Univariate data analyse')
+    left_ana, right_ana=columns(2)
+    with left_ana:
+        feature_choice=selectbox('Selecteer de kolom', options=raw.columns)
+        bins=slider('Aantal bins', min_value=4, max_value=100)
+    with right_ana:
+        plot_choice=selectbox('Plot keuze', options=["box",'violin', 'rug'])
+        coloryesno = checkbox('Colorscale')
+    figureee = px.histogram(raw.dropna(), x=feature_choice,
+                            marginal=plot_choice,  # or violin, rug
+                            hover_data=raw.columns, nbins=bins, color=(feature_choice if coloryesno else None))
+    plotly_chart(figureee)
+
+
+
+with gdp_merge:
+    header('Integreren van de BBP per hoofd data')
+    merge_l, merge_r = columns(2)
+    with merge_l:
+        option = radio('Selecteer visualisatie', options=['Kaart', 'Treemap'])
+    if option == 'Kaart':
+        with merge_r:
+            kaart_soort = radio("Is de aarde plat?", options=['Ja', 'Nee'])
+        if kaart_soort=='Ja':
+            plotly_chart(gdp_map_plat, use_container_width=True)
+        else:
+            plotly_chart(gdp_map_bol, use_container_width=True)
+    else:
+        plotly_chart(gdp_treemap, use_container_width=True)
+with feature_engineering:
+    header('Feature engineering')
+    write(ftfy.fix_encoding(t_feature_1))
+    f_left, f_right = columns(2)
+    with f_left:
+       pyplot(fig2, caption = 'Figuur x')
+    with f_right:
+        write(ftfy.fix_encoding(t_feature_2))
+    write(ftfy.fix_encoding(t_feature_3))
+    write(data.head())
+with visualisatie:
+    header('Visualisatie')
+    aantal_jobs = slider('Aantal beroepen', min_value=0, max_value=50, value=25)
+
+    kasper = px.bar(data_frame=final_kasper.head(aantal_jobs),
+                    x="Designation",
+                    y="Salary_eur",
+                    color='Designation',
+                    text_auto='.2s',
+                    labels={'Salary_eur': 'Salaris in euro per jaar', 'Designation': 'Beroep'}, height=500,
+                    title="Salaris per jaar per beroep in de US")
+    kasper.add_hline(y=data_kasper['gdp_euro'].mean(), line_width=3, line_dash="dash", line_color="black")
+    plotly_chart(kasper,use_container_width=True)
+
+    scatterplot_salaris, ax = plt.subplots()
+    f,g, h=columns(3)
+    with g:
+        markdown('Bedrijfsgrootte')
+        Klein=checkbox('Klein')
+        Medium=checkbox('Medium')
+        Groot=checkbox('Groot')
+    with h:
+        markdown('Jaar')
+        j2020=checkbox('2020')
+        j2021=checkbox('2021')
+        j2022=checkbox('2022')
+    with f:
+        markdown('Ervaring')
+        Starter = checkbox('Starter')
+        Medior = checkbox('Medior')
+        SeniorExpert = checkbox('Senior/Expert')
+        Executive=checkbox('Executive')
+
+    subdata = data
+
+    if not Klein:
+        subdata = subdata[subdata['Grootte'] != 'S']
+    if not Medium:
+        subdata = subdata[subdata['Grootte'] != 'M']
+    if not Groot:
+        subdata = subdata[subdata['Grootte'] != 'L']
+    if not j2020:
+        subdata = subdata[subdata['Jaar'] != 2020]
+    if not j2021:
+        subdata = subdata[subdata['Jaar'] != 2021]
+    if not Starter:
+        subdata = subdata[subdata['Experience'] != 'EN']
+    if not Medior:
+        subdata = subdata[subdata['Experience'] != 'MI']
+    if not SeniorExpert:
+        subdata = subdata[subdata['Experience'] != 'SE']
+    if not Executive:
+        subdata = subdata[subdata['Experience'] != 'EX']
+
+    x_scatter=subdata['GDP €']+np.random.normal(0,5000,len(subdata['GDP €']))
+    y_scatter=subdata['Salaris €']
+
+    ax.scatter(x_scatter,y_scatter,s=5,alpha=0.4)
+
+
+    ax.set_ylabel('Salaris euro')
+    ax.set_title('Scatterplot BBP per hoofd vs salaris')
+    ax.set_xlabel('BBP per hoofd')
+    #pyplot(scatterplot_salaris)
+    k,l=columns(2)
+    scatterplotly_x = k.selectbox('x-as', options=subdata.columns)
+    scatterplotly_y = l.selectbox('y-as', options=subdata.columns)
+
+    try:
+        distortion_x = float(k.text_input('Ruis-x', value='0'))
+        distortion_y = float(l.text_input('Ruis-y', value='0'))
+    except:
+        raise ValueError('Het moet een getal zijn')
+    opacity = k.slider('Doorzichtigheid', max_value=1000, min_value=0, value=1000, step=1)/1000
+    trendline = l.selectbox('Trendlijn', options=[None,'ols', 'lowess',  'expanding' ])
+    try:
+        scatterplotly = px.scatter(x=subdata[scatterplotly_x]+np.random.normal(0,distortion_x,len(subdata)),
+                                   y=subdata[scatterplotly_y]+np.random.normal(0,distortion_y,len(subdata)),
+                                   labels={'x':str(scatterplotly_x), 'y':str(scatterplotly_y)},
+                                   trendline=trendline,
+                                   opacity=opacity,
+                                   trendline_color_override='red')
+    except:
+        scatterplotly = px.bar(x=subdata[scatterplotly_x],
+                                   y=subdata[scatterplotly_y],
+                                   labels={'x': str(scatterplotly_x), 'y': str(scatterplotly_y)},
+                                   opacity=opacity)
+    plotly_chart(scatterplotly)
+
+with salary_pred:
+    header('De salaris voorspeller')
+    image(salary_image)
+    write('Benieuwd wat je gaat vedienen als je gaat werken in de data wereld? Stel jouw '
+          'toekomstige baan samen door de '
+          'onderstaande mini enquete in te vullen en '
+          'kom er achter was je salaris gaat worden!')
+
+    des = selectbox('Selecteer beroep', options=data.Designation.unique())
+    pred_left, pred_right = columns(2)
+    with pred_left:
+        ex = select_slider('Selecteer ervaring', options=['Entry', 'Medior', 'Senior/Expert', 'Executive'])
+        size = selectbox('Grootte bedrijf', options=['Groot', 'Medium', 'Klein'])
+        la_pe = selectbox('Land persoon', options=data['Land persoon'].unique())
+    with pred_right:
+        rat = slider('Thuiswerk ratio', min_value=0, max_value=100, step=50)
+        stat = selectbox('Selecteer dienstverband', options=['Fulltime', 'Parttime', 'Oproepbasis', 'Freelance'])
+        la_be = selectbox('Land bedrijf', options=data['Land bedrijf'].unique())
+
+    size = size_dict[size]
+    stat = stat_dict[stat]
+    ex = ex_dict[ex]
+    if button('Genereer'):
+        percent, salarisS = salary_prediction(des, ex, stat, la_pe, la_be, size, rat)
+        subheader(f'Het voorspelde salaris is {salarisS}, dat is {percent}% ten opzichte van het gdp')
