5c642d73caa08f59a474f0c46bdb6c8f88eafdf3
pythonista101 <123813803+pythonista101@users.noreply.github.com>
2023-03-08 12:43:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -219,7 +219,9 @@ with salary_pred:
         subheader(f'Het voorspelde salaris is {salarisS}, dat is {percent}% ten opzichte van het gdp')
     
     header('Discussie')
-    write(Discussie)
-    pyplot(piechartdiscussie)
+    fgf, gfg = columns(2)
+    
+    fgf.write(Discussie)
+    gfg.pyplot(piechartdiscussie)
     text("Deze website is gemaakt door: ....")
     image(qrcode)
