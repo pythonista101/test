e42b19de06f1a66b6b5a71f482344bcd48fe283b
pythonista101 <123813803+pythonista101@users.noreply.github.com>
2023-03-08 14:38:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ with visualisatie:
 
 with salary_pred:
     header('De salaris voorspeller')
-    image(salary_image)
+    #image(salary_image)
     write('Benieuwd wat je gaat vedienen als je gaat werken in de data wereld? Stel jouw '
           'toekomstige baan samen door de '
           'onderstaande mini enquete in te vullen en '
