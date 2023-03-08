53f372c893cdeaad8d4e7b964e9f7e48ad8b8011
pythonista101 <123813803+pythonista101@users.noreply.github.com>
2023-03-08 13:45:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -315,7 +315,7 @@ with salary_pred:
     header('Discussie')
     fgf, gfg = columns(2)
     with fgf:
-        write("")
+        write("Figuur 1")
         pyplot(piechartdiscussie)
     with gfg:
         write(Discussie)
