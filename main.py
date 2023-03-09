37afc9828ebbc16829ef767223dfb02cf78f264d
pythonista101 <123813803+pythonista101@users.noreply.github.com>
2023-03-09 16:29:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -108,14 +108,14 @@ plotly_chart(figureee)""", language='python')
     
     with brandon_l:
             
-        write(ftfy.fix_encoding('Voor de eerste bar plot wordt naar het aantal medewerkers gekeken' 
-                                    'per ervaringsniveau van de baan gedurende de jaren 2020-2022. En staat voor '
-                                    'entry-level oftewel junior met een instapniveau, Ex staat voor executive met manager '
-                                    'gerelateerde beroepen. Mi staat voor een middenklasser en de Se staat voor werkervaring '
-                                    'op expert/senior niveau. Als we het aantal medewerkers per ervaringsniveau voor de '
-                                    'jaren 2020 tot 2022 met elkaar vergelijken dan valt het op dat het aantal medewerkers'
-                                    'met een uitvoerend niveau in alle jaren in de minderheid zijn. Terwijl het aantal '
-                                    'middenklassers en expert/senioren hoger ligt. Alleen in het jaar 2020 is het aantal '
+        write(ftfy.fix_encoding('Voor de eerste bar plot wordt naar het aantal medewerkers gekeken' \n
+                                    'per ervaringsniveau van de baan gedurende de jaren 2020-2022. En staat voor '\n
+                                    'entry-level oftewel junior met een instapniveau, Ex staat voor executive met manager '\n
+                                    'gerelateerde beroepen. Mi staat voor een middenklasser en de Se staat voor werkervaring '\n
+                                    'op expert/senior niveau. Als we het aantal medewerkers per ervaringsniveau voor de '\n
+                                    'jaren 2020 tot 2022 met elkaar vergelijken dan valt het op dat het aantal medewerkers'\n
+                                    'met een uitvoerend niveau in alle jaren in de minderheid zijn. Terwijl het aantal '\n
+                                    'middenklassers en expert/senioren hoger ligt. Alleen in het jaar 2020 is het aantal '\n
                                     'junioren met instapniveau hoger. ')
         
         
